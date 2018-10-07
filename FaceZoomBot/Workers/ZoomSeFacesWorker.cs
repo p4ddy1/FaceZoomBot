1acb912e04a7dcf3a0db04afce7cefa9d6a2b162
Patric Kanngießer <mail@lpnw.de>
2018-10-07 17:52:32 +0200

    Changed Array2D to Matrix

--- FaceZoomBot/Workers/ZoomSeFacesWorker.cs
+++ FaceZoomBot/Workers/ZoomSeFacesWorker.cs
@@ -30,28 +30,27 @@ namespace FaceZoomBot.Workers
             try
             {
                 int facesFound;
-
+            
                 using (var detector = Dlib.GetFrontalFaceDetector())
                 using (var shapePredictor = ShapePredictor.Deserialize(Config.General.ShapePredictorPath))
                 using (var image = Storage.LoadImage(Job.ImagePath))
+                using(var convertedImage = ImageToMatrix(image))
                 {
-                    using (var convertedImage = ImageToArray2D(image))
+                    var detections = detector.Operator(convertedImage);
+                    facesFound = detections.Length;
+                    for (int i = 0; i < facesFound; i++)
                     {
-                        var detections = detector.Operator(convertedImage);
-                        facesFound = detections.Length;
-                        for (int i = 0; i < facesFound; i++)
+                        using (var shape = shapePredictor.Detect(convertedImage, detections[i]))
+                        using (var faceChipDetail = Dlib.GetFaceChipDetails(shape, 700))
+                        using (var faceChip = Dlib.ExtractImageChip<RgbPixel>(convertedImage, faceChipDetail))
                         {
-                            using (var shape = shapePredictor.Detect(convertedImage, detections[i]))
-                            using (var faceChipDetail = Dlib.GetFaceChipDetails(shape, 700))
-                            using (var faceChip = Dlib.ExtractImageChip<RgbPixel>(convertedImage, faceChipDetail))
+                            using (var face = MatrixToImage(faceChip))
                             {
-                                var face = Array2DToImage(faceChip);
                                 Storage.SaveFace(face, Job.ImagePath);
                             }
                         }
                     }
                 }
-
                 if (facesFound < 1)
                 {
                     if (Job.TelegramChat.IsPrivate)
@@ -72,24 +71,25 @@ namespace FaceZoomBot.Workers
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
+               Console.WriteLine(ex.Message);
+              return false;
+           }
 
             return true;
         }
 
-        private Array2D<RgbPixel> ImageToArray2D(Image<Rgb24> image)
+        private Matrix<RgbPixel> ImageToMatrix(Image<Rgb24> image)
         {
             var width = image.Width;
             var height = image.Height;
 
-            var array2D = new Array2D<RgbPixel>(height, width);
+            var matrix = new Matrix<RgbPixel>(height, width);
+
             for (int row = 0; row < height; row++)
             {
                 for (int col = 0; col < width; col++)
                 {
-                    array2D[row][col] = new RgbPixel(
+                    matrix[row,col] = new RgbPixel(
                         image[col, row].R,
                         image[col, row].G,
                         image[col, row].B
@@ -97,24 +97,24 @@ namespace FaceZoomBot.Workers
                 }
             }
 
-            return array2D;
+            return matrix;
         }
 
-        private Image<Rgb24> Array2DToImage(Array2D<RgbPixel> array2D)
+        private Image<Rgb24> MatrixToImage(Matrix<RgbPixel> matrix)
         {
-            var width = array2D.Columns;
-            var height = array2D.Rows;
+            var width = matrix.Columns;
+            var height = matrix.Rows;
 
             var image = new Image<Rgb24>(width, height);
-
+            
             for (int row = 0; row < height; row++)
             {
                 for (int col = 0; col < width; col++)
                 {
                     image[col, row] = new Rgb24(
-                        array2D[row][col].Red,
-                        array2D[row][col].Green,
-                        array2D[row][col].Blue
+                        matrix[row,col].Red,
+                        matrix[row,col].Green,
+                        matrix[row,col].Blue
                     );
                 }
             }

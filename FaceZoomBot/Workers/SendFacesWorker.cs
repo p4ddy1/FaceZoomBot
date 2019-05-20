b9c61db397eb2d98458b0bb98a532f20b7b9e0a2
Patric Kanngießer <mail@lpnw.de>
2019-05-20 22:35:49 +0200

    Send multiple pictures as album

--- FaceZoomBot/Workers/SendFacesWorker.cs
+++ FaceZoomBot/Workers/SendFacesWorker.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using FaceZoomBot.DataStorage;
 using FaceZoomBot.Jobs;
 using FaceZoomBot.Telegram;
 using SixLabors.ImageSharp.Formats.Jpeg;
-using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types;
 
 namespace FaceZoomBot.Workers
 {
@@ -41,17 +42,21 @@ namespace FaceZoomBot.Workers
         {
             try
             {
+                var faceImageList = new List<IAlbumInputMedia>();
+                
                 foreach (var face in Storage.GetAllFaceIdentifiersForImage(imagePath))
                 {
                     using (var faceImage = Storage.LoadFace(imagePath, face))
-                    using (var faceImageStream = new MemoryStream())
                     {
+                        var faceImageStream = new MemoryStream();
                         faceImage.Save(faceImageStream, new JpegEncoder());
                         faceImageStream.Seek(0, SeekOrigin.Begin);
-                        var inputOnlineFile = new InputOnlineFile(faceImageStream);
-                        await TelegramClient.BotClient.SendPhotoAsync(Job.TelegramChat.ChatId, inputOnlineFile);
+                        faceImageList.Add(new InputMediaPhoto(new InputMedia(faceImageStream, GenerateRandomFilename())));
                     }
                 }
+                
+                await TelegramClient.BotClient.SendMediaGroupAsync(faceImageList, Job.TelegramChat.ChatId);
+                
                 Storage.DeleteAllFacesForImage(Job.ImagePath);
                 Storage.DeleteImage(Job.ImagePath);
             }
@@ -59,6 +64,12 @@ namespace FaceZoomBot.Workers
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        private string GenerateRandomFilename()
+        {
+            var random = new Random();
+            return "face" + random.Next(1, 1000000);
         }
     }
 }
